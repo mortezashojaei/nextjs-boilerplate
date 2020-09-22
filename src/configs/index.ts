d54e4c2eeef46cc654b56ac2cb4e362d262bf42b
morteza shojaei <mshojaei179@gmail.com>
2020-09-22 15:15:53 +0330

    add enviroment variables

---
+++ src/configs/index.ts
@@ -0,0 +1,3 @@
+export const configs = {
+  BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,6 @@ export default function Home() {
   return (
     <Container>
       <h1>Home page</h1>
-
       <ul>
         <li>
           <Link href="sample">
